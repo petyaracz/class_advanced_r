dca98fd76c45ac032ac9a3300d1fb186774598a8
Peter Racz <pracz@cogsci.bme.hu>
2023-11-09 12:10:04 +0100

    l9

--- script/lesson8.R
+++ script/lesson8.R
@@ -8,12 +8,25 @@ library(psych)
 d = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/main/dat/lesson8.tsv')
 # d = read_tsv('~/Work/BME/HaladóRÓra/haladorrepo/dat/lesson8.tsv')
 
-# use pairs.plot to look at data
-
+# use pairs.panel to look at data
+pairs.panels(d)
 # what are the possible models?
+lm1 = glm(Survived ~ Class + Age + Sex, data = d, family = binomial)
+lm2 = glm(Survived ~ Class * Age + Sex, data = d, family = binomial)
+lm3 = glm(Survived ~ Class + Age * Sex, data = d, family = binomial)
+lm4 = glm(Survived ~ Class * Sex + Age, data = d, family = binomial)
 
 # what is the best model?
+plot(compare_performance(lm1, lm2, lm3, lm4, metrics = 'common'))
+test_wald(lm1,lm4)
+check_collinearity(lm4)
+count(d,Sex,Class)
 # does the best model meet the assumptions?
+check_model(lm4)
+plot(compare_performance(lm1, lm2, lm3, metrics = 'common'))
+check_collinearity(lm3)
+tidy(lm3, conf.int = TRUE, conf.level = 0.95)
+plot_model(lm3, 'pred', terms = c('Age','Sex','Class'))
 # why? why not?
 # what is the best model that does meet the assumptions?
 # what does this model predict?
---
+++ script/lesson9.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(lme4)
+library(broom.mixed)
+library(performance)
+library(sjPlot)
+library(psych)
+library(janitor)
+
