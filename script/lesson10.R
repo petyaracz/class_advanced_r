ae71a3d1945e7e7d5d1475b185c9fdccbe749de3
Peter Racz <pracz@cogsci.bme.hu>
2023-11-20 12:46:01 +0100

    l10

--- script/lesson10.R
+++ script/lesson10.R
@@ -12,11 +12,23 @@ d = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/main/
 # structure of d
 d
 
-
 # effects of analogy and rules on regularity
 d %>% 
   ggplot(aes(analogy,regular)) +
   geom_jitter(width = 0, height = .1) +
   geom_smooth(method = 'glm', method.args = list(family = 'binomial'), se = FALSE) +
   scale_y_continuous(breaks = c(0,1))
-  
+
+# per subject?
+
+# "ordinary" GLM?
+
+# "ordinary" GLM with participant factor?
+
+# "ordinary" GLM with participant interaction?
+
+# hierarchical GLM with participant grouping factor (random intercept)?
+
+# hierarchical GLM with participant grouping factor (random intercept) and random slope?
+
+# gods?
