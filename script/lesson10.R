7c724a94f58f32a6625c7803e3a91ae9494bf541
Peter Racz <pracz@cogsci.bme.hu>
2023-11-20 12:13:24 +0100

    l10

--- script/lesson10.R
+++ script/lesson10.R
@@ -6,3 +6,17 @@ library(performance)
 library(sjPlot)
 library(lme4)
 library(glmmTMB)
+
+d = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/main/dat/l10d1.tsv')
+
+# structure of d
+d
+
+
+# effects of analogy and rules on regularity
+d %>% 
+  ggplot(aes(analogy,regular)) +
+  geom_jitter(width = 0, height = .1) +
+  geom_smooth(method = 'glm', method.args = list(family = 'binomial'), se = FALSE) +
+  scale_y_continuous(breaks = c(0,1))
+  
