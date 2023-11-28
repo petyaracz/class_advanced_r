435bb22eec02c08048c4f046050d26dc24379897
Peter Racz <pracz@cogsci.bme.hu>
2023-11-28 09:08:14 +0100

    upsz

--- script/lesson11.R
+++ script/lesson11.R
@@ -4,4 +4,4 @@ library(performance)
 library(sjPlot)
 library(lme4)
 
-d = read_tsv("'https://raw.githubusercontent.com/petyaracz/class_advanced_r/main/dat/l11d1.tsv")
+d = read_tsv("https://raw.githubusercontent.com/petyaracz/class_advanced_r/main/dat/l11d1.tsv")
