4ccaf0a072a250f0e995a9e40e2ff175b9faff42
Péter Rácz <petermartonracz@gmail.com>
2024-10-14 19:56:13 +0200

    Update lesson4b.R

--- script/lesson4b.R
+++ script/lesson4b.R
@@ -11,10 +11,12 @@ library(sjPlot)
 
 # -- read -- #
 
-d1 = read_tsv('dat/l4d1b.tsv')
-d2 = read_tsv('dat/l4d2b.tsv')
-d3 = read_tsv('dat/l4d3b.tsv')
-d4 = read_tsv('dat/l4d4b.tsv')
+d1 = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/refs/heads/main/dat/l4d1b.tsv')
+d2 = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/refs/heads/main/dat/l4d2b.tsv')
+d3 = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/refs/heads/main/dat/l4d3b.tsv')
+d4 = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/refs/heads/main/dat/l4d4b.tsv')
+d5 = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/refs/heads/main/dat/l4d5b.tsv')
+d6 = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/refs/heads/main/dat/l4d6b.tsv')
 
 # -- ex1 -- #
 
