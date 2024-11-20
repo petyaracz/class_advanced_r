2f618d4413b1e1b89f3045240b3e93775d7b3893
Péter Rácz <petermartonracz@gmail.com>
2024-11-20 16:30:02 +0100

    Update lesson10b.R

--- script/lesson10b.R
+++ script/lesson10b.R
@@ -1,8 +1,21 @@
+# -- header -- #
+
+install.packages('maps')
+
 library(tidyverse)
 library(lme4)
 library(broom.mixed)
 library(performance)
 library(sjPlot)
+library(maps)
+
+world = map_data("world")
+tidy2 = partial(tidy, conf.int = T)
+
+# -- read -- #
 
 d1 = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/main/dat/anthropology1.tsv')
 d2 = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/main/dat/anthropology2.tsv')
+
+# -- gods -- #
+
