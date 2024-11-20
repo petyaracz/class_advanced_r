b4c90017526e96a136550e11c3596c6980d3679d
Péter Rácz <petermartonracz@gmail.com>
2024-11-20 16:26:22 +0100

    whoops

--- script/lesson10b.R
+++ script/lesson10b.R
@@ -4,4 +4,5 @@ library(broom.mixed)
 library(performance)
 library(sjPlot)
 
-d1 = read_tsv('')
+d1 = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/main/dat/anthropology1.tsv')
+d2 = read_tsv('https://raw.githubusercontent.com/petyaracz/class_advanced_r/main/dat/anthropology2.tsv')
